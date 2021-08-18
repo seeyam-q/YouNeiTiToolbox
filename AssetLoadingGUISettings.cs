ac9ca68752f61ff388ef0d993ff69277bc770c66
Siyuan Q <siyuan.qiu1@gmail.com>
2021-08-18 01:05:22 -0400

    feat(ExternalAssetLoader): add ExternalAssetLoader

---
+++ AssetLoadingGUISettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FortySevenE.ExternalAssetLoading
+{
+    [Serializable]
+    public class AssetLoadingGUISettings
+    {
+        public Sprite[] spriteAnimationArray;
+        public Color textColor = Color.black;
+        public Texture2D background;
+        public Color backgroundColor = Color.black;
+    }
+}
