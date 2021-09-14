587db6749f895b427fca20668c52d444a4a34773
Siyuan <siyuan.qiu1@gmail.com>
2021-09-13 22:58:15 -0400

    feat(bootstrapper): add bootstrapper

---
+++ Code/Editor/BootstrapDictionaryInspector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using FortySevenE.Bootstrapper;
+
+[CustomEditor(typeof(BootstrapDictionary))]
+public class BootstrapDictionaryInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Generate Bootstrap File"))
+        {
+            ((BootstrapDictionary)target).UpdateBootstrapFileFromSettingPopulateList();
+        }
+    }
+}
