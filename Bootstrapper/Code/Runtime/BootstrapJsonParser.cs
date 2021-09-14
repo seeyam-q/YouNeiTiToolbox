97963b8a770421accb8ee1f089830abd489dd383
Siyuan <siyuan.qiu1@gmail.com>
2021-09-13 22:58:15 -0400

    feat(bootstrapper): add bootstrapper

---
+++ Bootstrapper/Code/Runtime/BootstrapJsonParser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace FortySevenE.Bootstrapper
+{ 
+    public class BootstrapJsonParser: IBootstrapRawTextParser
+    {
+        private JsonSerializerSettings _jsonSerializerSettings;
+
+        public BootstrapJsonParser()
+        {
+            _jsonSerializerSettings = new JsonSerializerSettings();
+            _jsonSerializerSettings.Converters.Add(new StringEnumConverter());
+            _jsonSerializerSettings.Converters.Add(new WanzyeeStudio.Json.ColorConverter());
+        }
+
+        public bool TryGetSettingDictionary(string rawText, ref Dictionary<string, object> settingDictionary)
+        {
+            try
+            {
+                settingDictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(rawText);
+                return true;
+            }
+            catch
+            {
+                Debug.LogWarning("Cannot parse bootstrap.");
+            }
+            return false;
+        }
+
+        public bool TryParseRawText(string rawText, Type type, out object referenceValue)
+        {
+            try
+            {
+                referenceValue = JsonConvert.DeserializeObject(rawText, type, _jsonSerializerSettings);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+            }
+            referenceValue = null;
+            return false;
+        }
+
+        public string SerializeDictionary(Dictionary<string, object> value)
+        {
+            return JsonConvert.SerializeObject(value, Formatting.Indented, _jsonSerializerSettings);
+        }
+    }
+}
---
+++ Bootstrapper/Code/Runtime/Net3.5Compatibility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+#if !NET_4_6
+public static class FieldInfoExtensions
+{
+    public static T GetCustomAttribute<T>(this FieldInfo type, bool inherit = false) where T : Attribute
+    {
+        object[] attributes = type.GetCustomAttributes(inherit);
+        return attributes.OfType<T>().FirstOrDefault();
+    }
+}
+#endif
