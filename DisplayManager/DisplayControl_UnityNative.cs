6cb0c34ad94b8e93a5c0607199615ec033a38cf2
Siyuan <siyuan.qiu1@gmail.com>
2021-09-13 22:58:50 -0400

    feat(displaymanager): add DisplayManager

---
+++ DisplayManager/DisplayControl_UnityNative.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FortySevenE.DisplayManager
+{
+    public class DisplayControl_UnityNative : MonoBehaviour, IDisplayControl
+    {
+        Dictionary<int, DisplayWindow> _cacheDisplaySettingsAfterReload;
+
+        public void RefreshDisplayPointersAfterNewDisplayAdded()
+        {
+
+        }
+
+        public void SetPosition(int index, int left, int top, bool relativeToMonitor, int relativeMonitorIndex)
+        {
+            SetPositionAndSize(index, left, top, true, relativeMonitorIndex, 0, 0);
+        }
+
+        public void SetPositionAndSize(int index, int left, int top, bool relativeToMonitor, int relativeMonitorIndex, int width, int height)
+        {
+            UnityEngine.Display.displays[index].SetParams(width, height, left, top);
+        }
+
+        public void SetSize(int index, int width, int height)
+        {
+            //SetPositionAndSize(index, 0, 0, true, width, height);
+        }
+
+        public void SetWindowStyle(int index, WindowStyle displayStyle)
+        {
+            switch(displayStyle)
+            {
+                case WindowStyle.Borderless:
+                    Screen.fullScreenMode = FullScreenMode.MaximizedWindow;
+                    break;
+                case WindowStyle.FullMenuBar:
+                    Screen.fullScreenMode = FullScreenMode.Windowed;     
+                    break;
+                case WindowStyle.MenuBarNoResize:
+                    Screen.fullScreenMode = FullScreenMode.Windowed;
+                    break;
+            }
+        }
+    }
+}
---
+++ DisplayManager/DisplaySettings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FortySevenE.DisplayManager
+{
+    public enum DisplayControlApiType
+    {
+        OSSpecific,
+        UnityNative
+    }
+
+    public enum WindowStyle
+    {
+        Borderless,
+        MenuBarNoResize,
+        FullMenuBar
+    }
+
+	[Serializable]
+	public class DisplayWindow
+	{
+        public int relativeMonitorIndex;
+        public WindowStyle windowStyle;
+
+		public int left;
+		public int top;
+		public int width;
+		public int height;
+
+        public DisplayWindow(int left, int top, int width, int height, WindowStyle windowStyle)
+        {
+            this.left = left;
+            this.top = top;
+            this.width = width;
+            this.height = height;
+            this.windowStyle = windowStyle;
+        }
+    }
+
+    [Serializable]
+    public class DisplaySettings 
+    {
+        public DisplayControlApiType apiType;
+        // resize multi displays breaks Unity's UI event system (I love Unity!). Making it optional incase UI interaction is needed on multi-display
+        public bool resizeMultiDisplays;
+        public DisplayWindow[] displayList;
+    }
+}
