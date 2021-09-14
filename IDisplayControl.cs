80a2d809c8f9571a051e76263a518803d8427eb1
Siyuan <siyuan.qiu1@gmail.com>
2021-09-13 22:58:50 -0400

    feat(displaymanager): add DisplayManager

---
+++ IDisplayControl.cs
@@ -0,0 +1,11 @@
+﻿namespace FortySevenE.DisplayManager
+{
+    public interface IDisplayControl
+    {
+        void SetPositionAndSize(int index, int left, int top, bool relativeToMonitor, int relativeMonitorIndex, int width, int height);
+        void SetPosition(int index, int left, int top, bool relativeToMonitor, int relativeMonitorIndex);
+        void SetSize(int index, int width, int height);
+        void SetWindowStyle(int index, WindowStyle displayStyle);
+        void RefreshDisplayPointersAfterNewDisplayAdded();
+    }
+}
