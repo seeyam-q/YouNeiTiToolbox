64fea39fa7809282477cd2a10cd2145d6f826f4c
Seeyam <siyuan.qiu1@gmail.com>
2022-01-25 21:22:13 -0500

    feat(General): refactor Singleton - borrow the code from MyBox

--- Runtime/Singleton.cs
+++ Runtime/Singleton.cs
@@ -1,81 +1,44 @@
-//http://wiki.unity3d.com/index.php/Singleton
+// modified from https://github.com/Deadcows/MyBox/blob/master/Types/Singleton.cs
 
+using System;
 using UnityEngine;
 
 namespace FortySevenE
 {
-    /// <summary>
-    /// Inherit from this base class to create a singleton.
-    /// e.g. public class MyClassName : Singleton<MyClassName> {}
-    /// </summary>
-    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
-        // Check to see if we're about to be destroyed.
-        //private static bool _shuttingDown = false;
-        private static object _lock = new object();
-        private static T _instance;
+        //backward compatibility for old singleton naming
+        public static T X => Instance;
 
-        /// <summary>
-        /// Access singleton instance through this propriety.
-        /// </summary>
-        public static T X
+        public static T Instance
         {
             get
             {
-                //if (_shuttingDown)
-                //{
-                //    Debug.LogWarning("[Singleton] Instance '" + typeof(T) +
-                //        "' already destroyed. Returning null.");
-                //    return null;
-                //}
+                if (_instance == null) _instance = FindObjectOfType<T>();
+                if (_instance == null) Debug.LogError("Singleton of type : " + typeof(T).Name + " not found on scene");
 
-                lock (_lock)
-                {
-                    if (_instance == null)
-                    {
-                        // Search for existing instance.
-                        _instance = (T)FindObjectOfType(typeof(T));
-
-                        // Create new instance if one doesn't already exist.
-                        if (_instance == null)
-                        {
-                            // Need to create a new GameObject to attach the singleton to.
-                            var singletonObject = new GameObject();
-                            _instance = singletonObject.AddComponent<T>();
-                            singletonObject.name = typeof(T).ToString() + " (Singleton)";
-                        }
-
-                        if (_instance != null)
-                        {
-                            if (Application.isPlaying)
-                            {
-                                // Make instance persistent.
-                                DontDestroyOnLoad(_instance);
-                            }
-                        }
-                    }
-
-                    return _instance;
-                }
+                return _instance;
             }
         }
+        private static T _instance;
 
 
-        //private void OnApplicationQuit()
-        //{
-        //    if (Application.isPlaying)
-        //    {
-        //        _shuttingDown = true;
-        //    }
-        //}
-
-
-        //private void OnDestroy()
-        //{
-        //    if (Application.isPlaying)
-        //    {
-        //        _shuttingDown = true;
-        //    }
-        //}
+        /// <summary>
+        /// Use this function to cache instance and destroy duplicate objects.
+        /// Also use DontDestroyOnLoad if "persistent" is not set to false
+        /// </summary>
+        protected void InitializeSingleton(bool persistent = true)
+        {
+            if (_instance == null)
+            {
+                _instance = (T)Convert.ChangeType(this, typeof(T));
+                if (persistent) DontDestroyOnLoad(_instance);
+            }
+            else
+            {
+                Debug.LogWarning($"Another instance of Singleton<{typeof(T).Name}> detected on GO {name}. Destroyed", gameObject);
+                Destroy(this);
+            }
+        }
     }
 }
