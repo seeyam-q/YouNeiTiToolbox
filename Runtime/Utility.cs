0cbc47899077f6c5088cf88c525fa0447293f083
seeyam <seeyamq@gmail.com>
2025-06-21 01:42:24 -0400

    chore: add a generic lerp function to Utility

--- Runtime/Utility.cs
+++ Runtime/Utility.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -46,5 +47,65 @@ namespace FortySevenE
             float delta = value - range.x;
             return delta / (range.y - range.x);
         }
+        
+        public static T Lerp<T>(T start, T end, float t) where T : struct
+        {
+            t = Mathf.Clamp01(t);
+
+            if (typeof(T) == typeof(float))
+            {
+                // We must cast to object first, then to the specific type.
+                // This is how you work around C#'s strict generic type safety.
+                float s = (float)(object)start;
+                float e = (float)(object)end;
+                return (T)(object)Mathf.Lerp(s, e, t);
+            }
+
+            if (typeof(T) == typeof(Vector2))
+            {
+                Vector2 s = (Vector2)(object)start;
+                Vector2 e = (Vector2)(object)end;
+                return (T)(object)Vector2.Lerp(s, e, t);
+            }
+
+            if (typeof(T) == typeof(Vector3))
+            {
+                Vector3 s = (Vector3)(object)start;
+                Vector3 e = (Vector3)(object)end;
+                return (T)(object)Vector3.Lerp(s, e, t);
+            }
+
+            if (typeof(T) == typeof(Vector4))
+            {
+                Vector4 s = (Vector4)(object)start;
+                Vector4 e = (Vector4)(object)end;
+                return (T)(object)Vector4.Lerp(s, e, t);
+            }
+
+            if (typeof(T) == typeof(Color))
+            {
+                Color s = (Color)(object)start;
+                Color e = (Color)(object)end;
+                return (T)(object)Color.Lerp(s, e, t);
+            }
+            
+            if (typeof(ILerpable<T>).IsAssignableFrom(typeof(T)))
+            {
+                // Cast the 'start' value to the interface.
+                // The 'start' instance itself will perform the lerp.
+                ILerpable<T> s = (ILerpable<T>)start;
+            
+                // Call the interface method, passing 'end' as the target.
+                return s.Lerp(start, end, t);
+            }
+
+            // It's better to throw an exception for unsupported types than to fail silently.
+            throw new NotSupportedException($"The type '{typeof(T).Name}' is not supported by GenericLerp.");
+        }
+    }
+    
+    public interface ILerpable<T>
+    {
+        T Lerp(T start, T end, float amount);
     }
 }
